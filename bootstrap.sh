9b7aff395d9bee898f710751ec890dc84c860fc1
Your Name <you@example.com>
2020-07-15 16:44:45 +0100

    echo tests

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,23 @@
-##Disable IPv6
 GRUBFILE='/etc/default/grub'
+USER=oem
+SSHKEY=/home/$USER/.ssh/id_rsa
+
+##Disable IPv6
 echo Removing IPv6 functionality
 sudo sed -i -e 's/""/"ipv6.disable=1"/g'  /etc/default/grub
 sudo sed -i -e 's/quiet splash"/quiet splash ipv6.disable=1"/g'  $GRUBFILE
 
-
 ##Test IPv 6 config lines are in place
-echo Checking IPv6 lines are in grub config file
-grep "ipv6.disable=1" $GRUBFILE
+echo Checking IPv6 lines are in grub config file 
+grep "ipv6.disable=1" $GRUBFILE && echo ipv6 disabled || echo ipv6 not disabled 
 
 ##INSTALL ANSIBLE
 echo installing ansible and configuring ansible
 rm -rf ~/.cache
-FILE=/home/oem/.ssh/id_rsa
-if [ -f $FILE ]; then
-   echo "File $FILE already exists."
+if [ -f $SSHKEY ]; then
+   echo "File $SSHKEY already exists."
 else
-   echo "File $FILE does not exist. Creating."
+   echo "File $SSHKEY does not exist. Creating."
    ssh-keygen -t rsa -C "example@example.com"
 fi
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install python3-pip -y
@@ -25,14 +26,13 @@ sudo mkdir -p /etc/ansible
 echo localhost| sudo tee /etc/ansible/hosts
 
 #TEST ANSIBLE IS INSTALLED
-echo confirming ansible is installed...
-ansible -m ping all
+echo confirming ansible is installed... && ansible -m ping all && echo installed || echo not installed
 
 
 ##INSTALL GALAXY DEPENDANCIES
 sudo mkdir -p /etc/ansible/roles
 sudo mkdir -p /usr/share/ansible/roles
-echo APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 | sudo tee /etc/environment #circumnavigate apt error
+echo APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 | sudo tee /etc/environment #circumnavigate ansible apt error
 ansible-galaxy install darkwizard242.googlechrome
 ansible-galaxy install oefenweb.slack
 
