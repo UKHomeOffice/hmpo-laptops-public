972ed086b321ac8d5886f4b8d36cba4fd99273c6
Your Name <you@example.com>
2020-07-15 14:51:18 +0100

    ansible installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
+rm -rf ~/.cache
 ssh-keygen -t rsa -C "example@example.com"
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install python3-pip -y
+sudo pip3 install ansible
 sudo mkdir -p /etc/ansible
-echo localhost > /etc/ansible/hosts
+echo localhost| sudo tee /etc/ansible/hosts
 
+echo confirming ansible is installed...
+ansible -m ping all
