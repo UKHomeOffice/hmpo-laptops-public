2f5b56d479b034c1171e770a5d1564d6b309be49
Your Name <you@example.com>
2020-07-16 15:44:17 +0100

    removing unnecessary files

--- thing.sh
+++
@@ -1,6 +0,0 @@
-
-if [ $(sudo md5sum /home/ansible/.ssh/id_rsa.pub| awk '{print $1}') == $(sudo md5sum /home/ansible/.ssh/authorized_keys| awk '{print $1}') ]; then
-	echo "Keys match"
-else
-	echo "keys don't match"
-fi
